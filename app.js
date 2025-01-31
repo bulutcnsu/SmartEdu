78aed9fe66b2ba1a007970080455cea3b3336cfc
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2025-01-31 03:58:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(
     })
   );
 mongoose
-  .connect('mongodb://localhost/smartedu-db')
+  .connect('mongodb+srv://dbUser:dbUserPass123@cluster0.lmmul.mongodb.net/smartedu-db?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log('DB Connected!'));
 
   app.use('*', (req, res, next) => {
