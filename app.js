985c78621874951f0d38e435eda2fa4abaa75dc7
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2025-01-31 03:48:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ app.use('/users', authRoute);
 
 
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('App  started on port:', port);
 });
